04494ee7013f56a7c3f3110dae9416a0a75b4a5d
lshht911 <lshht911@naver.com>
2023-09-01 21:11:26 +0900

    프런트 단

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 const home = require("./app/src/routes/home"); // 라우트폴더 홈폴더의 index.js 를 읽어와줘
 // 앱 세팅
 app.set("views","./app/src/views");
-app.set("view engine","ejs")
+app.set("view engine","ejs");
+app.use(express.static(`${__dirname}/src/public`));
+
 
 
 app.use("/",home); 
